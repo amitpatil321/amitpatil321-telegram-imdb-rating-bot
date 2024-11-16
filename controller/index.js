373769f4a0b6431d63c9c7df6d2053b3f127e8ad
Amit <amitpatil321@gmail.com>
2024-11-17 00:44:08 +0530

    debug: vercel debug

--- controller/index.js
+++ controller/index.js
@@ -4,7 +4,8 @@ async function handler(req, method) {
   const { body } = req;
   if (body) {
     const messageObj = body.message;
-    await handleMessage(messageObj);
+    if (messageObj) await handleMessage(messageObj);
+    else console.log("messageObj is empty, Aborting!");
   } else {
     console.log("Message has no body, Aborting!");
   }
