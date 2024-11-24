9f83f98a8c7bb940d53df0f568964d219a881d38
Amit <amitpatil321@gmail.com>
2024-11-24 22:00:14 +0530

    feat: fresh start

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const port = 3000;
 app.use(express.json());
 
 app.post("*", async (req, res) => {
-  console.log("post", req);
+  console.log("post", req.body);
   // res.send(await handler(req, res));
 });
 
-app.get("*", async (req, res) => {
-  console.log("get", req);
-  // res.send(await handler(req, res));
-});
+// app.get("*", async (req, res) => {
+//   console.log("get", req.body);
+//   // res.send(await handler(req, res));
+// });
 
 app.listen(port, () => {
   console.log(`Movie rating app running on port: ${port}`);
