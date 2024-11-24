58268159bf4fc76d4a8b93e96c64beefd2bed342
Amit <amitpatil321@gmail.com>
2024-11-24 22:02:43 +0530

    feat: fresh start

--- controller/index.js
+++ controller/index.js
@@ -16,26 +16,23 @@ async function handler(req, res) {
   // Restrict to POST requests
   if (req.method !== "POST") {
     console.log(`Invalid request method`);
-  }
-
-  const { body } = req;
-
-  // Check for message body
-  if (body && body.message) {
-    try {
-      // Example: Uncomment and use your handler logic here
-      await handleMessage(body.message);
-      // return res
-      //   .status(200)
-      //   .send({ message: "Message processed successfully" });
-    } catch (err) {
-      console.error("Error handling message:", err.message); // Log error message
-      // return res.status(500).send({ error: "Internal Server Error" });
-    }
   } else {
-    console.log("Message body is missing or empty, Aborting!");
-    // return res.status(400).send({ error: "Bad Request" });
+    console.log("All good");
   }
+
+  // const { body } = req;
+
+  // if (body && body.message) {
+  //   try {
+  //     await handleMessage(body.message);
+  //   } catch (err) {
+  //     console.error("Error handling message:", err.message); // Log error message
+  //     return res.status(500).send({ error: "Internal Server Error" });
+  //   }
+  // } else {
+  //   console.log("Message body is missing or empty, Aborting!");
+  //   return res.status(400).send({ error: "Bad Request" });
+  // }
 }
 
 module.exports = { handler };
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
-// const { handler } = require("./controller/index");
+const { handler } = require("./controller/index");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
 app.post("*", async (req, res) => {
-  console.log("post", req.body);
-  // res.send(await handler(req, res));
+  res.send(await handler(req, res));
 });
 
 // app.get("*", async (req, res) => {
