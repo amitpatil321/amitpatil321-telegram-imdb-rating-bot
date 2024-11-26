6f7ad64a55f1e3f40992ebf9f9c87fc2bc587409
Amit <amitpatil321@gmail.com>
2024-11-26 11:46:57 +0530

    feat: fresh start

--- controller/index.js
+++ controller/index.js
@@ -15,18 +15,17 @@ const { handleMessage } = require("./lib/telegram");
 async function handler(req, res) {
   if (req.method !== "POST") {
     console.log(`Invalid request method`);
-  } else console.log("All good");
+  }
 
   const { body } = req;
 
   if (body && body.message) {
-    console.log("Has body");
-    // try {
-    //   await handleMessage(body.message);
-    // } catch (err) {
-    //   console.error("Error handling message:", err.message); // Log error message
-    //   return res.status(500).send({ error: "Internal Server Error" });
-    // }
+    try {
+      await handleMessage(body.message);
+    } catch (err) {
+      console.error("Error handling message:", err.message); // Log error message
+      return res.status(500).send({ error: "Internal Server Error" });
+    }
   } else {
     console.log("Message body is missing or empty, Aborting!");
     return res.status(400).send({ error: "Bad Request" });
--- controller/lib/telegram.js
+++ controller/lib/telegram.js
@@ -3,12 +3,15 @@ const movieController = require("../../controller/lib/moviesDB");
 
 async function handleMessage(messageObj) {
   const messageText = messageObj?.text || "";
+  console.log(messageText);
   // if its a bot command
   if (messageText.charAt(0) === "/") {
+    console.log("Command detected");
     const command = messageText.substr(1);
     this.handleCommands(messageObj, command);
   } else {
     // else its a movie name
+    console.log("Movies name received");
     console.log(messageObj);
     movieController.handleMovie(messageObj);
   }
