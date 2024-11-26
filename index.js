594da45d5fb477fae617ece3059e4699c90c5c79
Amit <amitpatil321@gmail.com>
2024-11-26 12:12:23 +0530

    feat: add favicon

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.static("public"));
 
 app.post("*", async (req, res) => {
   res.send(await handler(req, res));
