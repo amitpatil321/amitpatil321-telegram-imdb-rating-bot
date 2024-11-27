1e50c1413676995044c559bcfecaa614b4608ab1
Amit <amitpatil321@gmail.com>
2024-11-27 13:43:50 +0530

    fix: using telegram bot api

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const WEBHOOK_URL = `https://${process.env.VERCEL_URL}/`;
 bot.setWebHook(WEBHOOK_URL);
 
 app.post("/", (req, res) => {
-  console.log("Request received: " + JSON.stringify(req));
+  console.log("Request received: " + JSON.stringify(req.body));
   bot.processUpdate(req.body); // Pass incoming updates to the bot
   res.sendStatus(200);
 });
