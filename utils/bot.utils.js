52883838214beb19784d13bd218317a596531e64
Amit <amitpatil321@gmail.com>
2024-11-28 00:16:02 +0530

    fix: testing on vercel

--- utils/bot.utils.js
+++ utils/bot.utils.js
@@ -1,4 +1,14 @@
 const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
+(async () => {
+  try {
+    const result = await bot.deleteWebhook();
+    console.log("Webhook deleted:", result);
+    bot.startPolling();
+  } catch (error) {
+    console.error("Error deleting webhook:", error);
+  }
+})();
+
 module.exports = bot;
