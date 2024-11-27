40ee94c44ab9d21d8d74c7aeecf45a3bd4a458bb
Amit <amitpatil321@gmail.com>
2024-11-27 15:34:46 +0530

    fix: using telegram bot api

--- utils/movie.utils.js
+++ utils/movie.utils.js
@@ -2,6 +2,7 @@ const moviesApi = require("../api/movies.api");
 
 module.exports = {
   async handleCommands(messageObj) {
+    const chatId = msg.chat.id;
     const movieName = messageObj?.text || "";
     const movieInfo = await moviesApi.getMovie(movieName);
     console.log("0");
@@ -9,7 +10,13 @@ module.exports = {
       if (movieInfo?.data?.total_results > 1) {
         console.log(movieInfo.data.results[0]);
       } else {
-        console.log(movieInfo.data.results[0]);
+        let movie = movieInfo.data.results[0];
+        let messageText =
+          `*Title:* ${movie?.original_title}\n` +
+          `*IMDb Rating:* ${movie?.vote_average.toFixed(1) || "NA"}\n` +
+          `*Release Date:* ${movie.release_date.split("-")[0]}\n` +
+          `*Genres:* ${movie?.genres?.map((each) => each?.name).join(", ")}`;
+        bot.sendMessage(chatId, messageText);
       }
     }
   },
